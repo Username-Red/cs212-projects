c401bf5395ccb8e9c0521f48c95cbb6b24681073
Username-Red <ashkestrel@gmail.com>
2023-11-02 01:24:13 +0200

    Completing ArraysTester.cs project

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -39,7 +39,23 @@ public static class ArraysTester {
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return new double[0]; // replace this return statement with your own
+        // Use a for loop that loops an amount of times equal to 'length', 
+        // each time adding the value of 'number' to a variable, 
+        // replaces the new variable to the appropriate position in the list of multiples, 
+        // and then returns the final list after the loop is finished
+
+
+        double multiple = 0;
+        // List<double> multiples = new List<double>();
+        double[] doom = new double[length];
+        for(int i = 0; i < length; i++) {
+            multiple += number;
+            // multiples.Add(multiple);
+            doom[i] = multiple;
+
+        }
+
+        return doom; // replace this return statement with your own
     }
     
     /// <summary>
@@ -56,6 +72,43 @@ public static class ArraysTester {
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // take the 'data' list and split it at the 'amount' index, forming two separate lists/arrays
+        // take the two new lists/arrays and combine them back to front, allowing for the 'rotation' effect
+
+
+        List<int> data1 = new List<int>();
+        List<int> data2 = new List<int>();
+
+        for(int i = amount; i < data.Count(); i++) {
+            data2.Add(data[i]);
+        };
+
+        for(int i = 0; i < amount; i++) {
+            data1.Add(data[i]);
+        };
+
+        data2.AddRange(data1);
+        
+        data.Clear();
+
+        data.AddRange(data2);
+
+        // List<int> data2 = new List<int>();
+
+        // for (int i = amount; i < data.Count(); i++) {
+        //     data2.Add(data[i]);
+        // }
+
+        // for (int i = 0; i < amount; i++) {
+        //     data2.Add(data[i]);
+        // }
+
+        // data = data2;
+        
+
+
+
 
     }
 }
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,32 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return new int[0];
+        //Set up some variables to use for list navigation
+        var result = new int[select.Length];
+        var l1Idx = 0;
+        var l2Idx = 0;
+
+        //open a for loop to loop through the select list
+        for (var i = 0; i < select.Length; i++)
+        {
+            //if the select list item is equal to 1, use list1 to add to result
+            if (select[i] == 1) {
+                result[i] = list1[l1Idx++];
+            }
+                
+            //if the select list item is equal to 2, use list2 to add to the result
+            else if (select[i] == 2) {
+                result[i] = list2[l2Idx++];
+            }
+
+            //if there is something wrong with the select list, throw an error to the screen explaining the problem
+            else{
+                Console.WriteLine("An error has occured with the selector list. Please ensure that the selector list only contains 1s and 2s.");
+            }
+                
+        }
+
+        //return the final result
+        return result;
     }
 }
